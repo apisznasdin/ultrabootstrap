da20f4ca6c4c0ea481ba74ee280de1093ad12a52
Phantom Themes <phantomthemes@gmail.com>
2018-09-10 16:19:57 +0545

    Fixed Search

--- searchform.php
+++ searchform.php
@@ -2,18 +2,18 @@
 /**
  * The template for displaying search forms in Underscores.me
  *
- * @package understrap
+ * @package ultrabootstrap
  */
 
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="assistive-text" for="s"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
+	<label class="assistive-text" for="s"><?php esc_html_e( 'Search', 'ultrabootstrap' ); ?></label>
 	<div class="input-group">
 		<input class="field form-control" id="s" name="s" style="margin-bottom:5px;" type="text"
-			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
+			placeholder="<?php esc_attr_e( 'Search &hellip;', 'ultrabootstrap' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
 			<input class="submit btn btn-danger" id="searchsubmit" name="submit" type="submit"
-			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
+			value="<?php esc_attr_e( 'Search', 'ultrabootstrap' ); ?>">
 	</span>
 	</div>
 </form>
